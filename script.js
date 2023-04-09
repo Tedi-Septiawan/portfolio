877327a93fa50e6a8f635a0ebe14adfeadfa9d0a
Tedi-Septiawan <tediseptiawan32@gmail.com>
2023-04-09 18:37:00 +0700

    tamba file

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Rider Trail Adventure", "Desain Grafis", "Editor Video"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
